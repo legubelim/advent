a8a5aaf872d89b9e27a1042e9680745c6e81d002
legubelim <glegrand@gmx.net>
2024-11-23 16:21:38 +0100

    day 4 part 2

--- 2023/day_4.py
+++ 2023/day_4.py
@@ -60,3 +60,28 @@ print(score)
 #%%
 
 ## part 2    
+
+cards = {}
+for line in lines:
+    print(f"line: {line}")
+    m = prog_line.match(line)
+    card_id = int(m.group(1))
+    nb_copies = cards.get(card_id, 0) + 1
+    cards[card_id] = nb_copies
+    #print(m.group(2))
+    winning_numbers = [int(nb) for nb in prog_space.split(m.group(2))]
+    #print(m.group(3))
+    #print([nb for nb in prog_space.split(m.group(3))])
+    my_numbers = [int(nb) for nb in prog_space.split(m.group(3))]
+    #print(m.group(3))
+    #print(my_numbers)
+    n = len([nb for nb in my_numbers if nb in winning_numbers])
+    print(f"nb winning nbs: {n}")
+    print(list(range(card_id + 1, card_id + 1 + n)))
+    for cid in range(card_id + 1, card_id + 1 + n):
+        if cid <= len(lines):
+            cards[cid] = cards.get(cid, 0) + nb_copies
+        
+print(cards)
+print(sum(cards.values()))
+
